644d296fa9fc759a9816a449e95d11aba8e580f4
Avik <aviknandi@yahoo.com>
2018-07-26 13:24:20 +0530

    Wrote comment for each section of the code

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
+#Create file for first 20 element in periodic elementPeriodicTable
+
 file = open("elementPeriodicTable.txt", "w+")
 file.write("Hydrogen\nHelium\nLithium\nBeryllium\nBoron\nCarbon\nNitrogen\nOxygen\n\
 Fluorine\nNeon\nSodium\nMagnesium\nAluminum\nSilicon\nPhosphorus\nSulfur\nChlorine\n\
 Argon\nPotassium\nCalcium\n")
 file.close()
+
+#Put all the elements of file into a list
 
 file = open("elementPeriodicTable.txt", "r+")
 file.seek(0)
@@ -15,12 +19,12 @@ while newLine:
 print(elementList)
 
 
-
-# element =[]
-# while len(element)<5:
-#     inputForElement= input("Enter your Element: ")
-#     if inputForElement in element:
-#         print(inputForElement+" was already entered          <--no duplicates allowed ")
-#     else:
-#         element.append(inputForElement)
-# print element
+# Collect input of 5 unique element names
+element =[]
+while len(element)<5:
+    inputForElement= input("Enter your Element: ")
+    if inputForElement in element:
+        print(inputForElement+" was already entered          <--no duplicates allowed ")
+    else:
+        element.append(inputForElement)
+print element
